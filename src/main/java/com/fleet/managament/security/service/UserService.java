abc407002bdf7be729ec915c686a949ccb51c8eb
gichukipitar <ptahgichuki@yahoo.com>
2024-08-05 14:11:21 +0300

    User service class

---
+++ src/main/java/com/fleet/managament/security/service/UserService.java
@@ -0,0 +1,23 @@
+package com.fleet.managament.security.service;
+
+import com.fleet.managament.security.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            public UserDetails loadUserByUsername(String username) {
+                return userRepository.findByUsername(username)
+                        .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+            }
+        };
+    }
+}
