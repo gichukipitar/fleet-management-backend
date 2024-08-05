5c011936dd0d92113fe1ea4ab8cddd73f4ac18e5
gichukipitar <ptahgichuki@yahoo.com>
2024-08-05 13:37:11 +0300

    User entity class updates

---
+++ src/main/java/com/fleet/managament/security/entity/User.java
@@ -0,0 +1,70 @@
+package com.fleet.managament.security.entity;
+
+import com.fleet.managament.uac.dtos.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "\"User\"")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String email;
+    private String phoneNumber;
+    private String password;
+    private Role role;
+    private Date createDate;
+    private String createBy;
+    private Date modifiedDate;
+    private String modifiedBy;
+    private boolean active;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        // email in our case
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
---
+++ src/main/java/com/fleet/managament/uac/dtos/Role.java
@@ -0,0 +1,6 @@
+package com.fleet.managament.uac.dtos;
+
+public enum Role {
+    SUPER_ADMIN,
+    SUPER_USER
+}
