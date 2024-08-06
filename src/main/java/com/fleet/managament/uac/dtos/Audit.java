3e3db8a20e97d90ed58e2ec6a58550ade0607a0e
gichukipitar <ptahgichuki@yahoo.com>
2024-08-06 09:07:23 +0300

    Audit class

---
+++ src/main/java/com/fleet/managament/uac/dtos/Audit.java
@@ -0,0 +1,10 @@
+package com.fleet.managament.uac.dtos;
+
+import java.time.LocalDateTime;
+
+public class Audit {
+    private LocalDateTime createdOn;
+    private String createdBy;
+    private LocalDateTime modifiedOn;
+    private String modifiedBy;
+}
