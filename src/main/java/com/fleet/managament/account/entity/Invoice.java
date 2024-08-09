50e6260d0c54f6713fc111436c2596110b4d1c86
gichukipitar <ptahgichuki@yahoo.com>
2024-08-09 12:44:28 +0300

    Invoice entity class

---
+++ src/main/java/com/fleet/managament/account/entity/Invoice.java
@@ -0,0 +1,34 @@
+package com.fleet.managament.account.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+public class Invoice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date invoiceDate;
+    @ManyToOne
+    @JoinColumn(name = "invoiceStatusId", insertable = false, updatable = false)
+    private InvoiceStatus invoiceStatus;
+    private Long invoiceStatusId;
+    @ManyToOne
+    @JoinColumn(name = "clientId", insertable = false, updatable = false)
+    private Client client;
+    private Long clientId;
+    private String remarks;
+}
