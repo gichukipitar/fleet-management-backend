d2ea7eb60c742c534b0c6ce68483cf890ba47aa6
gichukipitar <ptahgichuki@yahoo.com>
2024-08-12 09:46:46 +0300

    Client class

---
+++ src/main/java/com/fleet/managament/parameters/entity/Client.java
@@ -0,0 +1,41 @@
+package com.fleet.managament.parameters.entity;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String address;
+    private String city;
+    private String county;
+
+    @ManyToOne
+    @JoinColumn (name = "countryId", insertable = false, updatable = false)
+    private Country country;
+    private Long countryId;
+
+    @ManyToOne
+    @JoinColumn(name = "stateId", insertable = false, updatable = false)
+    private County county;
+    private Long stateId;
+
+    private String details;
+
+}
