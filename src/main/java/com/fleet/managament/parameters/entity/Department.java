52f5f4014864b3c517e716b66d89d455df4225a0
gichukipitar <ptahgichuki@yahoo.com>
2024-08-12 10:52:11 +0300

    Department class

---
+++ src/main/java/com/fleet/managament/parameters/entity/Department.java
@@ -0,0 +1,27 @@
+package com.fleet.managament.parameters.entity;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "managerId", insertable = false, updatable = false)
+    private Employee manager;
+    private Long managerId;
+}
