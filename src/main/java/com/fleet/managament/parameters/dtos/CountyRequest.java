970b5a87f3995f1075a6ef707b64c46a4c3b6f7f
gichukipitar <ptahgichuki@yahoo.com>
2024-08-13 08:45:23 +0300

    County request and response class essential updates

--- src/main/java/com/fleet/managament/parameters/dtos/CountyRequest.java
+++ src/main/java/com/fleet/managament/parameters/dtos/CountyRequest.java
@@ -1,4 +1,17 @@
 package com.fleet.managament.parameters.dtos;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+
+@Data
+@Builder
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class CountyRequest {
+    private String name;
+    private String capital;
+    private String code;
+
 }
--- src/main/java/com/fleet/managament/parameters/dtos/CountyResponse.java
+++ src/main/java/com/fleet/managament/parameters/dtos/CountyResponse.java
@@ -1,4 +1,16 @@
 package com.fleet.managament.parameters.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CountyResponse {
+    private String name;
+    private String capital;
+    private String code;
 }
