1b2696311872197a0d20b83ea18a3e9f2bb6fd3a
gichukipitar <ptahgichuki@yahoo.com>
2024-08-16 00:21:10 +0300

    country repository

---
+++ src/main/java/com/fleet/managament/parameters/repository/CountryRepository.java
@@ -0,0 +1,10 @@
+package com.fleet.managament.parameters.repository;
+
+import com.fleet.managament.parameters.entity.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CountryRepository extends JpaRepository<Country, Long> {
+
+}
