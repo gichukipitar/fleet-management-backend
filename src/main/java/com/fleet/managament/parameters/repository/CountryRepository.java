c158b62c983f3691b3b7a1a3a1442b1ec07ce817
gichukipitar <ptahgichuki@yahoo.com>
2024-08-16 09:29:50 +0300

    Country Repository updates

--- src/main/java/com/fleet/managament/parameters/repository/CountryRepository.java
+++ src/main/java/com/fleet/managament/parameters/repository/CountryRepository.java
@@ -2,9 +2,16 @@ package com.fleet.managament.parameters.repository;
 
 import com.fleet.managament.parameters.entity.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CountryRepository extends JpaRepository<Country, Long> {
+
+    @Query(value = "select c from Country c where " +
+    "c.description LIKE %?1% or c.capitalCity like %?1%")
+    List<Country> findByKeyword(String description);
 
 }
